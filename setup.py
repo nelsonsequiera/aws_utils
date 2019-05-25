01de23aaf9d0dbede5efa04c6427786854e5060f
nelsonsequiera <nelsonsequiera@gmail.com>
2019-05-26 01:47:24 +0530

    Add - S3 and DynamoDB

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import re
+import setuptools
+
+
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+
+def get_version():
+    init = open(os.path.join(ROOT, 'aws_utils', '__init__.py')).read()
+    return VERSION_RE.search(init).group(1)
+
+
+setuptools.setup(
+    name='aws_utils_for_lambda',
+    version=get_version(),
+    scripts=[],
+    author="Nelson Sequiera",
+    author_email="nelsonsequiera@gmail.com",
+    description="AWS utils",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nelsonsequiera/aws_utils",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "boto3==1.9.120"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+    ],
+)
